f483a78cc5630863a99ed8c7ddfb9f031b2a7d2c
Choi.SY <sollly0829@gmail.com>
2021-08-04 10:27:50 +0900

    Ex01 method 기본 설명

---
+++ src/day09_method/Ex01.java
@@ -0,0 +1,28 @@
+package day09_method;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	/*
+	 # class
+	  - 사용자 정의 자료형(사용자가 만들고 싶은데로 만들어 사용하는 자료형)
+	  - 재사용 가능
+	 # 객체
+	  - 변수를 객체라고 부른다 (현재까지의 사용하는 우리의 의미)
+	  - class 자료형으로 변수를 만드는 것
+	 # 메소드(method)
+	  - 기능을 말한다 (보통 소괄호()가 있으면 기능이라고 보면 된다)
+	  - 보편적으로 메소드 또는 함수라고 말한다
+	  - 메소드는 클래스 내부에서 정의하는 것을 말한다
+	  - 함수는 클래스 없이 독립적으로 사용하는 것 
+	 */
+	
+	public static void main(String[] args) {
+		
+		String name, addr, phNum,birth;
+		int age, stNum;
+		
+		
+	}
+}
