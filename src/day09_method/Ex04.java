7832cc06d85dcca20c934c5e7ac5741a0524f645
Choi.SY <sollly0829@gmail.com>
2021-08-04 12:42:49 +0900

    Ex04,Ex04_Method Method 생성 시 매개변수 사용

---
+++ src/day09_method/Ex04.java
@@ -0,0 +1,13 @@
+package day09_method;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		
+		Ex04_Method ex = new Ex04_Method();
+		//ex.sumFunc(24, 98);		// 인자값(파라미터)
+		int a=24, b=98;
+		ex.sumFunc(a, b);
+		ex.test("문자열",100);
+	}
+}
---
+++ src/day09_method/Ex04_Method.java
@@ -0,0 +1,14 @@
+package day09_method;
+
+public class Ex04_Method {
+
+	public void sumFunc(int n1, int n2) {
+		int sum = 0;
+		sum = n1+n2;
+		System.out.println("두 합 : " + sum);
+	}
+	
+	public void test(String s, int num) {
+		System.out.println("결과 : " + s);
+	}
+}
