c66f6b440b723845f617c7aad4fa40022c666ec4
Choi.SY <sollly0829@gmail.com>
2021-08-04 11:14:24 +0900

    Ex02,Ex02_Method Method기본

---
+++ src/day09_method/Ex02.java
@@ -0,0 +1,13 @@
+package day09_method;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		Ex02_Method ex = new Ex02_Method();
+		ex.test();
+		
+		String s = "aaa";
+		// ctrl + class에 마우스를 가져다 대면 관련 내용을 볼 수 있다
+	}
+}
---
+++ src/day09_method/Ex02_Method.java
@@ -0,0 +1,18 @@
+package day09_method;
+
+public class Ex02_Method {
+	
+	// method에서 합성어를 사용할 땐 camel 형식으로 이름 설정한다
+	public void test() {	//public void testFunction() 
+		System.out.println("test 기능 입니다");
+	}
+
+	// public int(반환타입) test(메소드 이름:사용자지정) int n(매개변수:아규먼트)
+	public int test(int n) {
+		//기능 코드 구현
+		// return n : 돌려주고자 하는 값 또는 해당 메소드를 종료할 때 사용
+		return n;
+		
+	}
+	
+}
